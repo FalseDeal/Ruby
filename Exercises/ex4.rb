8999d51f523387b15cf3540b74952e412ee7ee36
FalseDeal <drfalsedeal@gmail.com>
2015-09-05 15:37:50 +0300

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   Exercises/ex4.rb
    	new file:   Exercises/ex5.rb

---
+++ Exercises/ex4.rb
@@ -0,0 +1,9 @@
+#ex4.rb
+#variables
+
+x = 100
+shinigami = "death" #variable shinigami with the value
+#death
+
+puts x
+puts shinigami
---
+++ Exercises/ex5.rb
@@ -0,0 +1,20 @@
+#ex5.rb
+#sumit
+puts "." * 10 #wtf
+
+end1 = "wut"
+end2 = "wl"
+
+print end1 + end2
+puts end1 + end2
+puts end1 + " " + end2
+
+print end1 + end2
+print end1 + end2
+
+puts "what's that code do #{'snow'}"
+puts "test #{'random'}"
+
+test = "txt"
+puts "enter #{test}"
+puts "enter #{'test'}"
